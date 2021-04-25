2ea9dc15ab11facf3a74d356c2480f7ebc4cd0cd
Arifa Nafees <46690572+Arifa99@users.noreply.github.com>
2021-04-25 20:25:23 +0530

    Added new scenes

--- IVARA Class 8/Assets/Scripts/PlaceModel.cs
+++ IVARA Class 8/Assets/Scripts/PlaceModel.cs
@@ -38,12 +38,5 @@ public class PlaceModel : MonoBehaviour
         }
 
 
-         // On pressing Escape key Exit the application
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-         Application.Quit();
-        }
-
-
     }
 }
